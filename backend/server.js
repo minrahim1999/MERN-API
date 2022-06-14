3685e7d3c3eda1e6a32f1a9d72e336d0c7a90cf3
Muhaimin Rahim <minrahim1999@gmail.com>
2022-06-14 10:55:26 +0800

    MERN-API

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const dotenv = require('dotenv').config()
+const express = require('express')
+const { errorHandler } = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(PORT, () => {
+  console.log(`SERVER STARTED ON ${PORT}`)
+})
